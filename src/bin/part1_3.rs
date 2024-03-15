ae2a2d877509af304f3c00a23501c8694601515c
Cory <cory1993johnson@gmail.com>
2024-03-15 10:46:08 +0100

    final answer part1_3 and listend to Jon..

--- src/bin/part1_3.rs
+++ src/bin/part1_3.rs
@@ -1,89 +1,12 @@
-use std::collections::HashMap;
 use crypto_pals::*;
 
 fn main() {
-    
-}
-
-fn single_byte_xor(mess: Vec<u8>, key: u8) -> Result<Vec<u8>, &'static str> {
-    let output = mess.iter()
-        .map(|x| {
-            x ^ key
-        })
-        .collect::<Vec<u8>>();
-    Ok(output)
-}
-
-fn vec_to_string(input: Vec<u8>) -> Result<String, &'static str> {
-    Ok(input.iter().map(|x| {
-        *x as char
-    })
-    .collect::<String>())
-}
-
-
-
-#[test]
-fn set1_chal1() {
     let input = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
-
-    // sourced from ol' reliable.. ChatGPT :D
-    let frequency_map: HashMap<&str, f32> = [
-        (" ", 17.0),
-        ("E", 10.6),
-        ("T", 7.7),
-        ("A", 7.5),
-        ("O", 7.1),
-        ("N", 6.7),
-        ("I", 6.6),
-        ("S", 5.9),
-        ("R", 5.9),
-        ("H", 4.8),
-        ("L", 4.0),
-        ("D", 3.4),
-        ("C", 3.1),
-        ("U", 2.7),
-        ("M", 2.5),
-        ("F", 2.4),
-        ("P", 2.0),
-        ("G", 1.9),
-        ("W", 1.7),
-        ("Y", 1.5),
-        ("B", 1.5),
-        ("V", 1.0),
-        ("K", 0.8),
-        ("X", 0.2),
-        ("J", 0.2),
-        ("Q", 0.1),
-        ("Z", 0.1),
-    ]
-    .iter()
-    .cloned()
-    .collect();
 
     let bytes = string_to_bytes(input).unwrap();
 
-    let mut scores = (0..=255).into_iter()
-        .map(|key| {
-            let mut score: f32 ;
-            score = 0.0;
-            //decode (XOR)
-            let temp_vec = single_byte_xor(bytes.clone(), key).unwrap();
-
-            //score it
-            score = temp_vec.iter()
-                .map(|x|{
-                    // TODO: ensure value is upper case
-                    let key = *x as char;
-                    frequency_map.get(key.to_string().as_str()).unwrap_or(&0.0)
-                })
-                .sum();
-
-            //provide score and key to return tuple
-            (key, score)
-        }).collect::<Vec<(u8,f32)>>();
-    scores.sort_by(|(_, v1), (_, v2)| v2.total_cmp(v1));
-    //println!("{:?}", scores);
+    let scores = score_single_byte(bytes.clone()).unwrap();
+    println!("{:?}", scores);
 
     let high_score = scores[0].0;
     println!("High Score: {}",high_score);
@@ -93,4 +16,16 @@ fn set1_chal1() {
             high_score)
         .unwrap())
         .unwrap());
+}
+
+#[test]
+fn set1_chal3() {
+    // test derived from finding the answer in main XD
+    let input = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736");
+
+    let bytes = string_to_bytes(input).unwrap();
+    let scores = score_single_byte(bytes.clone()).unwrap();
+    let high_score = scores[0].0;
+
+    assert_eq!(high_score, 88);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,39 @@
-use std::u8;
+use std::{collections::HashMap, u8};
 use base64::{Engine as _, engine::general_purpose};
+use lazy_static::lazy_static;
+
+// sourced from ol' reliable.. ChatGPT :D
+lazy_static![
+    static ref FREQENCY_MAP: HashMap<&'static str, f32> = [
+        (" ", 17.0),
+        ("E", 10.6),
+        ("T", 7.7),
+        ("A", 7.5),
+        ("O", 7.1),
+        ("N", 6.7),
+        ("I", 6.6),
+        ("S", 5.9),
+        ("R", 5.9),
+        ("H", 4.8),
+        ("L", 4.0),
+        ("D", 3.4),
+        ("C", 3.1),
+        ("U", 2.7),
+        ("M", 2.5),
+        ("F", 2.4),
+        ("P", 2.0),
+        ("G", 1.9),
+        ("W", 1.7),
+        ("Y", 1.5),
+        ("B", 1.5),
+        ("V", 1.0),
+        ("K", 0.8),
+        ("X", 0.2),
+        ("J", 0.2),
+        ("Q", 0.1),
+        ("Z", 0.1)
+        ].iter().copied().collect();
+    ];
 
 pub fn hex_to_base64(input: String) -> Result<String,&'static str> {
     let bytes = string_to_bytes(input).unwrap();
@@ -17,4 +51,48 @@ pub fn string_to_bytes(input: String) -> Result<Vec<u8>, &'static str> {
         };
     };
     Ok(bytes)
+}
+
+pub fn single_byte_xor(mess: Vec<u8>, key: u8) -> Result<Vec<u8>, &'static str> {
+    let output = mess.iter()
+        .map(|x| {
+            x ^ key
+        })
+        .collect::<Vec<u8>>();
+    Ok(output)
+}
+
+pub fn vec_to_string(input: Vec<u8>) -> Result<String, &'static str> {
+    Ok(input.iter().map(|x| {
+        *x as char
+    })
+    .collect::<String>())
+}
+
+pub fn score_single_byte(bytes: Vec<u8>) -> Result<Vec<(u8,f32)>,&'static str> {
+    
+    let mut scores = (0..=255).into_iter()
+        .map(|key| {
+            let mut score: f32 ;
+            score = 0.0;
+            //decode (XOR)
+            let temp_vec = single_byte_xor(bytes.clone(), key).unwrap();
+
+            //score it
+            score = temp_vec.iter()
+                .map(|x|{
+                    // TODO: ensure value is upper case
+                    let mut key = *x as char;
+                    if key >= (97 as char) && key <= (122 as char) {
+                        key = ((key as u8) - 32) as char;
+                    }
+                    FREQENCY_MAP.get(key.to_string().as_str()).unwrap_or(&0.0)
+                })
+                .sum();
+
+            //provide score and key to return tuple
+            (key, score)
+        }).collect::<Vec<(u8,f32)>>();
+    scores.sort_by(|(_, v1), (_, v2)| v2.total_cmp(v1));
+    Ok(scores)
 }
