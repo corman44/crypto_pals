d47bf103dae95a3cf4856c94fcc0694af704dc88
ceej <cory1993johnson@gmail.com>
2024-03-21 21:59:47 +0100

    what is big O anyways

--- benches/bench_libs.rs
+++ benches/bench_libs.rs
@@ -1,23 +1,20 @@
 use crypto_pals::*;
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use divan::bench;
 
-pub fn criterion_single_xor(c: &mut Criterion) {
-    let bytes: Vec<u8> = vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    c.bench_with_input(BenchmarkId::new("single_xor", 16), &bytes, |f, b| 
-        f.iter(|| 
-            single_byte_xor(b.to_vec(), 75)
-        )
-    );
+pub fn main() {
+    divan::main();
 }
 
-pub fn criterion_score_single_byte(c: &mut Criterion) {
-    let bytes: Vec<u8> = vec![0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-    c.bench_with_input(BenchmarkId::new("score_single_bytes", 16), &bytes, |f, b| 
-        f.iter(|| 
-            score_single_byte(b.to_vec())
-        )
-    );
+#[bench]
+pub fn bench_single_xor() {
+    let mess: Vec<u8> = Vec::from("This is a Test Message for Benchmarking");
+    let _ = single_byte_xor(mess, 150);
 }
 
-criterion_group!(benches, criterion_single_xor, criterion_score_single_byte);
-criterion_main!(benches);
+#[bench]
+pub fn bench_score_single() {
+    let mess: Vec<u8> = Vec::from("This is a Test Message for Benchmarking");
+    let _ = score_single_byte(mess);
+}
+
+//#[divan::bench(args )]
--- justfile
+++ justfile
@@ -1,6 +1,11 @@
-# justfile containing shortcuts for cryptopals :D
+### justfile containing shortcuts for cryptopals :D
+
+bench libs:
+    cargo bench --bench bench_libs >> bench_libs.txt
 check part:
     cargo check --bin {{part}}
+lint part:
+    cargo clippy -p {{part}}
 new part:
     cp src/template src/bin/{{part}}.rs
 test part:
