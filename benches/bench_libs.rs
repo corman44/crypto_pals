fa9033507f96a7d0a9b71c854b0ee8e9d3a5e8e7
cory <cory1993johnson@gmail.com>
2024-04-14 08:46:29 +0200

    part1_8 done

--- benches/bench_libs.rs
+++ benches/bench_libs.rs
@@ -5,13 +5,13 @@ pub fn main() {
     divan::main();
 }
 
-#[bench]
+#[divan::bench]
 pub fn bench_single_xor() {
     let mess: Vec<u8> = Vec::from("This is a Test Message for Benchmarking");
     let _ = single_byte_xor(mess, 150);
 }
 
-#[bench]
+#[divan::bench]
 pub fn bench_score_single() {
     let mess: Vec<u8> = Vec::from("This is a Test Message for Benchmarking");
     let _ = score_single_byte(mess);
@@ -23,3 +23,4 @@ pub fn bench_repeating_key_xor() {
     let key: Vec<u8> = Vec::from("T#ST");
     let _ = repeating_key_xor(mess, key);
 }
+
---
+++ src/bin/part1_8.rs
@@ -0,0 +1,43 @@
+use std::{collections::HashSet, fs::read_to_string};
+
+use crypto_pals::*;
+
+/*
+Detect AES in ECB mode
+In this file are a bunch of hex-encoded ciphertexts.
+One of them has been encrypted with ECB.
+Detect it.
+Remember that the problem with ECB is that it is stateless and deterministic; the same 16 byte plaintext block will always produce the same 16 byte ciphertext.
+ */
+
+fn main() {
+    let filename = "inputs/input_1_8.txt";
+    let lines: Vec<String> = read_to_string(filename).expect("unable to read from input file")
+        .lines()
+        .map(|line| line.to_string())
+        .collect();
+    let byte_lines = lines.iter().map(|line| line.to_bytes()).collect::<Vec<Vec<u8>>>();
+
+    let mut unique = byte_lines.iter().filter(|line| !contains_duplicate(line.to_vec()));
+    let first_unique = unique.next().unwrap();
+    println!("len: {}, {:x?}", first_unique.len(), first_unique);
+}
+
+pub fn contains_duplicate(ciphertext: Vec<u8>) -> bool {
+    let split_buffer = ciphertext.chunks(16);
+    let mut unique = HashSet::new();
+    split_buffer.into_iter().all(|x| unique.insert(x))
+}
+
+#[cfg(test)]
+mod tester {
+    use super::*;
+
+    #[test]
+    fn test_contains_dup() {
+        let text_dup = "0123456789ABCDEF22222222222222220123456789ABCDEF";
+        let text_nondup = "0123456789ABCDEF22222222222222220123456783ABCDEF";
+        assert!(contains_duplicate(text_dup.as_bytes().to_vec()));
+        assert!(contains_duplicate(text_nondup.as_bytes().to_vec()));
+    }
+}
